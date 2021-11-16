bb7604a24522cd57a6033fb7332669c011dc67ee
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-15 21:52:36 -0600

    Added ability to tally score and announce final winner

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable import/extensions */
 /* eslint-disable arrow-body-style */
 const playerOptions = ['rock', 'paper', 'scissors'];
 const computerOptions = ['Rock', 'Paper', 'Scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 const computerPlay = () => {
   // To generate a random integer between minimum and maximum:
@@ -20,34 +23,54 @@ const playRound = (playerSelection, computerSelection = computerPlay()) => {
     return 'Please select rock, paper, or scissors.';
   }
 
+  // Format the playerSelection
   let playerSelectionFormatted = playerSelection.toLowerCase();
   playerSelectionFormatted = playerSelection[0].toUpperCase()
     + playerSelectionFormatted.slice(1, playerSelectionFormatted.length);
 
   if (playerSelectionFormatted === computerSelection) {
-    return 'Tie! Both played ' + playerSelectionFormatted;
+    return `Tie! Both played ${playerSelectionFormatted}`;
   }
   if (playerSelectionFormatted === 'Rock') {
     if (computerSelection === 'Scissors') {
+      playerScore++;
       return 'You win! Rock beats Scissors';
     }
+    computerScore++;
     return 'You lose! Paper beats Rock';
   }
   if (playerSelectionFormatted === 'Paper') {
     if (computerSelection === 'Rock') {
+      playerScore++;
       return 'You win! Paper beats Rock';
     }
+    computerScore++;
     return 'You lose! Scissors beats Paper';
   }
   if (playerSelectionFormatted === 'Scissors') {
     if (computerSelection === 'Paper') {
+      playerScore++;
       return 'You win! Scissors beats Paper';
     }
+    computerScore++;
     return 'You lose! Rock beats Scissors';
   }
   return 'Invalid round';
 };
 
-let playerSelection = 'SCISSORS';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+const game = () => {
+  // Play 5 rounds of rock paper scissors
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt('Please select rock, paper, or scissors.');
+    alert(playRound(playerSelection, computerPlay()));
+  }
+  if (playerScore === computerScore) {
+    return 'The game ended in a tie!';
+  }
+  if (playerScore > computerScore) {
+    return 'You won the game!';
+  }
+  return 'You lost the game!';
+};
+
+game();
