62f82ba9082d3dcfeca64aff8867a2ad21b18467
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-21 20:56:37 -0600

    Began refactoring to be played in the browser instead of console

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,33 @@
+/* eslint-disable no-alert */
 /* eslint-disable no-plusplus */
 /* eslint-disable import/extensions */
 /* eslint-disable arrow-body-style */
-const playerOptions = ['rock', 'paper', 'scissors'];
+// const playerOptions = ['rock', 'paper', 'scissors'];
 const computerOptions = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
+const pointsToWin = 5;
+
+// HELPER FUNCTIONS
+const handlePlayerWins = () => {
+  const message = document.querySelector('span.message');
+  message.textContent = `Player has won the game by winning ${pointsToWin} points!`;
+};
+
+const handleComputerWins = () => {
+  const message = document.querySelector('span.message');
+  message.textContent = `Computer has won the game by winning ${pointsToWin} points!`;
+};
+
+const resetGame = () => {
+  playerScore = 0;
+  computerScore = 0;
+  const message = document.querySelector('span.message');
+  message.textContent = '';
+};
 
 const computerPlay = () => {
-  // To generate a random integer between minimum and maximum:
+  // Generate random integer between minimum and maximum:
   // https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
   const maximum = 2;
   const minimum = 0;
@@ -15,62 +35,87 @@ const computerPlay = () => {
   return computerOptions[index];
 };
 
+const updateScoreBoard = () => {
+  const playerScoreDisplay = document.querySelector('span.playerScore');
+  const computerScoreDisplay = document.querySelector('span.computerScore');
+  playerScoreDisplay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
+};
+
+const checkGameScore = () => {
+  if (playerScore === pointsToWin) {
+    handlePlayerWins();
+    // resetGame();
+  }
+  if (computerScore === pointsToWin) {
+    handleComputerWins();
+    // resetGame();
+  }
+};
+
+// GAMEPLAY FUNCTIONS
 const playRound = (playerSelection, computerSelection = computerPlay()) => {
-  if (typeof playerSelection !== 'string') {
-    return 'Please select rock, paper, or scissors.';
+  let message = '';
+  if (playerSelection === computerSelection) {
+    message = `Tie! Both played ${playerSelection}.`;
   }
-  if (!playerOptions.includes(playerSelection.toLowerCase())) {
-    return 'Please select rock, paper, or scissors.';
-  }
-
-  // Format the playerSelection
-  let playerSelectionFormatted = playerSelection.toLowerCase();
-  playerSelectionFormatted = playerSelection[0].toUpperCase()
-    + playerSelectionFormatted.slice(1, playerSelectionFormatted.length);
-
-  if (playerSelectionFormatted === computerSelection) {
-    return `Tie! Both played ${playerSelectionFormatted}`;
-  }
-  if (playerSelectionFormatted === 'Rock') {
+  if (playerSelection === 'Rock') {
     if (computerSelection === 'Scissors') {
       playerScore++;
-      return 'You win! Rock beats Scissors';
+      message = 'Player won! Rock beats Scissors.';
+    } else {
+      computerScore++;
+      message = 'Player lost! Paper beats Rock.';
     }
-    computerScore++;
-    return 'You lose! Paper beats Rock';
   }
-  if (playerSelectionFormatted === 'Paper') {
+  if (playerSelection === 'Paper') {
     if (computerSelection === 'Rock') {
       playerScore++;
-      return 'You win! Paper beats Rock';
+      message = 'Player won! Paper beats Rock.';
+    } else {
+      computerScore++;
+      message = 'Player lost! Scissors beats Paper.';
     }
-    computerScore++;
-    return 'You lose! Scissors beats Paper';
   }
-  if (playerSelectionFormatted === 'Scissors') {
+  if (playerSelection === 'Scissors') {
     if (computerSelection === 'Paper') {
       playerScore++;
-      return 'You win! Scissors beats Paper';
+      message = 'Player won! Scissors beats Paper.';
+    } else {
+      computerScore++;
+      message = 'Player lost! Rock beats Scissors.';
     }
-    computerScore++;
-    return 'You lose! Rock beats Scissors';
   }
-  return 'Invalid round';
+  updateScoreBoard();
+  return message;
 };
 
-const game = () => {
+// Handle button clicks
+let playerSelection = '';
+const buttons = document.querySelectorAll('button.rpsButton');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    playerSelection = e.target.textContent;
+    const message = document.querySelector('span.message');
+    message.textContent = playRound(playerSelection, computerPlay());
+    checkGameScore();
+  });
+});
+
+// const game = () => {
+  // let playerSelection = '';
   // Play 5 rounds of rock paper scissors
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Please select rock, paper, or scissors.');
-    alert(playRound(playerSelection, computerPlay()));
-  }
-  if (playerScore === computerScore) {
-    return 'The game ended in a tie!';
-  }
-  if (playerScore > computerScore) {
-    return 'You won the game!';
-  }
-  return 'You lost the game!';
-};
+  // for (let i = 0; i < 5; i++) {
+  //   const playerSelection = prompt('Please select rock, paper, or scissors.');
+  //   alert(playRound(playerSelection, computerPlay()));
+  // }
+  // if (playerScore === computerScore) {
+  //   return 'The game ended in a tie!';
+  // }
+  // if (playerScore > computerScore) {
+  //   return 'You won the game!';
+  // }
+  // return 'You lost the game!';
+// };
 
-game();
+// game();
