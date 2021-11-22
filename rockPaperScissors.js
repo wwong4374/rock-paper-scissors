93bff655de7f04ff1795e15799c49ac1f645a6ea
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-22 00:04:13 -0600

    Now able to play via UI in browser

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,21 +9,48 @@ let computerScore = 0;
 const pointsToWin = 5;
 
 // HELPER FUNCTIONS
+const updateScoreBoard = () => {
+  const playerScoreDisplay = document.querySelector('span.playerScore');
+  const computerScoreDisplay = document.querySelector('span.computerScore');
+  playerScoreDisplay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
+};
+
+const resetScoreBoard = () => {
+  playerScore = 0;
+  computerScore = 0;
+  updateScoreBoard();
+  const message = document.querySelector('span.message');
+  message.textContent = 'Let\'s play!';
+};
+
+const handleGameEnd = () => {
+  // resetScoreBoard();
+  const buttons = document.querySelectorAll('button.rpsButton');
+  buttons.forEach((button) => {
+    button.removeEventListener('click', handleRPSButtonClick);
+  });
+};
+
 const handlePlayerWins = () => {
   const message = document.querySelector('span.message');
-  message.textContent = `Player has won the game by winning ${pointsToWin} points!`;
+  message.textContent = 'Player wins!';
+  handleGameEnd();
 };
 
 const handleComputerWins = () => {
   const message = document.querySelector('span.message');
-  message.textContent = `Computer has won the game by winning ${pointsToWin} points!`;
+  message.textContent = 'Computer wins';
+  handleGameEnd();
 };
 
-const resetGame = () => {
-  playerScore = 0;
-  computerScore = 0;
-  const message = document.querySelector('span.message');
-  message.textContent = '';
+const checkGameScore = () => {
+  if (playerScore === pointsToWin) {
+    handlePlayerWins();
+  }
+  if (computerScore === pointsToWin) {
+    handleComputerWins();
+  }
 };
 
 const computerPlay = () => {
@@ -35,35 +62,18 @@ const computerPlay = () => {
   return computerOptions[index];
 };
 
-const updateScoreBoard = () => {
-  const playerScoreDisplay = document.querySelector('span.playerScore');
-  const computerScoreDisplay = document.querySelector('span.computerScore');
-  playerScoreDisplay.textContent = playerScore;
-  computerScoreDisplay.textContent = computerScore;
-};
-
-const checkGameScore = () => {
-  if (playerScore === pointsToWin) {
-    handlePlayerWins();
-    // resetGame();
-  }
-  if (computerScore === pointsToWin) {
-    handleComputerWins();
-    // resetGame();
-  }
-};
-
 // GAMEPLAY FUNCTIONS
 const playRound = (playerSelection, computerSelection = computerPlay()) => {
   let message = '';
   if (playerSelection === computerSelection) {
     message = `Tie! Both played ${playerSelection}.`;
   }
+
   if (playerSelection === 'Rock') {
     if (computerSelection === 'Scissors') {
       playerScore++;
       message = 'Player won! Rock beats Scissors.';
-    } else {
+    } else if (computerSelection === 'Paper') {
       computerScore++;
       message = 'Player lost! Paper beats Rock.';
     }
@@ -72,7 +82,7 @@ const playRound = (playerSelection, computerSelection = computerPlay()) => {
     if (computerSelection === 'Rock') {
       playerScore++;
       message = 'Player won! Paper beats Rock.';
-    } else {
+    } else if (computerSelection === 'Scissors') {
       computerScore++;
       message = 'Player lost! Scissors beats Paper.';
     }
@@ -81,7 +91,7 @@ const playRound = (playerSelection, computerSelection = computerPlay()) => {
     if (computerSelection === 'Paper') {
       playerScore++;
       message = 'Player won! Scissors beats Paper.';
-    } else {
+    } else if (computerSelection === 'Rock') {
       computerScore++;
       message = 'Player lost! Rock beats Scissors.';
     }
@@ -90,32 +100,25 @@ const playRound = (playerSelection, computerSelection = computerPlay()) => {
   return message;
 };
 
-// Handle button clicks
-let playerSelection = '';
-const buttons = document.querySelectorAll('button.rpsButton');
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    playerSelection = e.target.textContent;
-    const message = document.querySelector('span.message');
-    message.textContent = playRound(playerSelection, computerPlay());
-    checkGameScore();
+const handleRPSButtonClick = (e) => {
+  const playerSelection = e.target.textContent;
+  const message = document.querySelector('span.message');
+  message.textContent = playRound(playerSelection, computerPlay());
+  checkGameScore();
+};
+
+const startNewGame = () => {
+  resetScoreBoard();
+  // let playerSelection = '';
+  const buttons = document.querySelectorAll('button.rpsButton');
+  buttons.forEach((button) => {
+    button.addEventListener('click', handleRPSButtonClick);
   });
+};
+
+const newGameButton = document.querySelector('button.newGameButton');
+newGameButton.addEventListener('click', () => {
+  startNewGame();
 });
 
-// const game = () => {
-  // let playerSelection = '';
-  // Play 5 rounds of rock paper scissors
-  // for (let i = 0; i < 5; i++) {
-  //   const playerSelection = prompt('Please select rock, paper, or scissors.');
-  //   alert(playRound(playerSelection, computerPlay()));
-  // }
-  // if (playerScore === computerScore) {
-  //   return 'The game ended in a tie!';
-  // }
-  // if (playerScore > computerScore) {
-  //   return 'You won the game!';
-  // }
-  // return 'You lost the game!';
-// };
-
-// game();
+startNewGame();
